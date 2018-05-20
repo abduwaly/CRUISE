7f2720f50c543659193fcee582a5d19c85be318b
arsay <arsay@foxmail.com>
2018-05-20 11:19:37 +0800

    upload all local resources to remote;

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    devServer: {
+      contentBase: './dist',
+      watchContentBase: true
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'assets'
+    },
+    module: {
+        rules: [{
+            test: /\.(scss|css)$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        },
+            // {
+            // test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+            // loader: 'file-loader?path=../assets/font-icons/font'
+            // }
+        ]
+    },
+
+};
